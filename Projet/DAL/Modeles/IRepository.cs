f7385611ffcb14ebc8100b75f458f51d88c2b258
Quentin <quentin.james@ecoles-epsi.net>
2025-03-20 12:41:38 +0100

    #76 #77 Ajout Repo généricité

---
+++ Projet/DAL/Modeles/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DAL.Interfaces.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetListAsync();
+
+        Task<T?> GetByIdAsync(int id);
+
+        Task<T?> PostAsync(T entity);
+
+        Task<T?> UpdateByIdAsync(int id, T entity);
+
+        Task<bool> DeleteAsync(int id);
+
+        IQueryable<T> Find(Expression<Func<T, bool>>? predicate = null,
+            Expression<Func<T, IProperty>>? navigationPropertyPath = null,
+            bool asNoTracking = true);
+    }
+}
