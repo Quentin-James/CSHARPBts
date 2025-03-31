a9eaee1507452267086c086d2fae44a5ad82e5a7
Quentin <quentin.james@ecoles-epsi.net>
2025-03-31 11:17:42 +0200

    #109 #108  Ajout du controller groupes spectacles et correction de son dto pour inclure le tarif et la programmation

---
+++ Projet/WebApplication1/Controllers/GroupesSpectaclesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DAL.Modeles;
+using Services.DTOs;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupesSpectaclesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GroupesSpectaclesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GroupesSpectacleDto>> CreateGroupesSpectacle([FromBody] GroupesSpectacleDto groupesSpectacleDto)
+        {
+            if (groupesSpectacleDto == null)
+                return BadRequest("Les données du groupe de spectacle sont invalides.");
+
+            var tarifExiste = await _context.TypesTarifs.AnyAsync(t => t.TarifId == groupesSpectacleDto.TarifId);
+            var programmationExiste = await _context.Programmations.AnyAsync(p => p.ProgrammationId == groupesSpectacleDto.ProgrammationId);
+
+            if (!tarifExiste)
+                return BadRequest("Le tarif spécifié n'existe pas.");
+            if (!programmationExiste)
+                return BadRequest("La programmation spécifiée n'existe pas.");
+
+            var groupesSpectacle = new GroupesSpectacle
+            {
+                GroupeId = groupesSpectacleDto.GroupeId,
+                NomGroupe = groupesSpectacleDto.NomGroupe
+            };
+
+            _context.GroupesSpectacles.Add(groupesSpectacle);
+            await _context.SaveChangesAsync(); 
+
+            return CreatedAtAction(nameof(CreateGroupesSpectacle), new { id = groupesSpectacle.GroupeId }, groupesSpectacleDto);
+        }
+    }
+}
